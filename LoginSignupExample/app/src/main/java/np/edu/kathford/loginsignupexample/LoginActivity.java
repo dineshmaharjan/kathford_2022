29d8cf1f08d8c9041d99c8f4f3c9bb03cc19bd86
Dinesh Maharjan <www.maharjandinesh@gmail.com>
2025-11-11 08:14:28 +0545

    finish singup flow

--- LoginSignupExample/app/src/main/java/np/edu/kathford/loginsignupexample/LoginActivity.java
+++ LoginSignupExample/app/src/main/java/np/edu/kathford/loginsignupexample/LoginActivity.java
@@ -1,10 +1,12 @@
 package np.edu.kathford.loginsignupexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -16,6 +18,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
 
+    private TextView signupTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         userNameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         loginButton = findViewById(R.id.loginButton);
+        signupTextView = findViewById(R.id.signupTextView);
 
 //        loginButton.setOnClickListener(this);
 
@@ -42,6 +47,16 @@ public class LoginActivity extends AppCompatActivity {
                     passwordEditText.setText(null);
                 }
 
+            }
+        });
+
+        signupTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //routing or navigation
+                Intent intent = new Intent(LoginActivity.this,
+                        SignupActivity.class);
+                startActivity(intent);
             }
         });
 
--- LoginSignupExample/app/src/main/java/np/edu/kathford/loginsignupexample/SignupActivity.java
+++ LoginSignupExample/app/src/main/java/np/edu/kathford/loginsignupexample/SignupActivity.java
@@ -2,15 +2,21 @@ package np.edu.kathford.loginsignupexample;
 
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class SignupActivity extends AppCompatActivity {
     private EditText fullNameEditText, mobileNUmberEditText, passwordEditText;
@@ -18,6 +24,11 @@ public class SignupActivity extends AppCompatActivity {
     private Spinner nationalitySpinner;
     private CheckBox termsAndConditionCheckBox;
     private Button signupButton;
+
+    private String fullName, mobileNumber, password;
+    private String gender;
+    private String nationality;
+    private boolean isTermsChecked = false;
 
 
     @SuppressLint("MissingInflatedId")
@@ -34,6 +45,67 @@ public class SignupActivity extends AppCompatActivity {
         nationalitySpinner = findViewById(R.id.nationalitySpinner);
         termsAndConditionCheckBox = findViewById(R.id.termsAndConditionCheckBox);
         signupButton = findViewById(R.id.signupButton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fullName = fullNameEditText.getText().toString().trim();
+                mobileNumber = mobileNUmberEditText.getText().toString().trim();
+                password = passwordEditText.getText().toString().trim();
+
+                if(!fullName.isEmpty() && !mobileNumber.isEmpty()
+                        && !password.isEmpty() && !gender.isEmpty()
+                        && !nationality.isEmpty() && isTermsChecked){
+                    Toast.makeText(SignupActivity.this, "Signup success",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+
+                }else{
+                    Toast.makeText(SignupActivity.this,
+                            "All fields are required",
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        genderRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (R.id.maleRadioButton == checkedId) {
+                    gender = "Male";
+                } else if (R.id.femaleRadioButton == checkedId) {
+                    gender = "Female";
+                } else {
+                    gender = "Other";
+                }
+
+            }
+        });
+
+        nationalitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                nationality = nationalitySpinner.getSelectedItem().toString();
+//                String[] nationalities=getResources().getStringArray(R.array.nationality);
+//                nationality = nationalities[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        termsAndConditionCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isTermsChecked = isChecked;
+
+            }
+        });
+
 
     }
 }
