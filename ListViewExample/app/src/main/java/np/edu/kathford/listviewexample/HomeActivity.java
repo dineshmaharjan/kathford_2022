9d9521cd0775566ab62585664c66a4dd91838784
Dinesh Maharjan <www.maharjandinesh@gmail.com>
2025-12-21 08:16:36 +0545

    simple gridview

--- ListViewExample/app/src/main/java/np/edu/kathford/listviewexample/HomeActivity.java
+++ ListViewExample/app/src/main/java/np/edu/kathford/listviewexample/HomeActivity.java
@@ -1,6 +1,8 @@
 package np.edu.kathford.listviewexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
@@ -9,9 +11,40 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HomeActivity extends AppCompatActivity {
 
     private Button listViewButton, gridViewButton, recylerViewButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_layout);
+        listViewButton = findViewById(R.id.listViewButton);
+        gridViewButton = findViewById(R.id.griViewViewButton);
+        recylerViewButton = findViewById(R.id.recyclerViewButton);
+
+        listViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent listViewIntent = new Intent(HomeActivity.this,
+                        MainActivity.class);
+                startActivity(listViewIntent);
+
+            }
+        });
+
+        gridViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gridViewIntent = new Intent(HomeActivity.this,
+                        MovieGridViewActivity.class);
+                startActivity(gridViewIntent);
+
+            }
+        });
+
+        recylerViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
